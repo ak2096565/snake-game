b2b07e60dbc96f51e145af6d3b80caa6239cfce0
Abhishek kumar das <ak2096565@gmail.com>
2025-08-11 23:49:57 +0530

    Fix: move fill and update inside game loop in main.py

--- code/main.py
+++ code/main.py
@@ -1,2 +1,22 @@
 from setting import*
 
+class Main:
+    def __init__(self):
+        
+        # general
+        pygame.init()
+        self.screen=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+        
+            self.screen.fill(LIGHT_GREEN)
+            pygame.display.update()
+                
+        
+main=Main()
+main.run()
