51239ac560dd8cc541afb28fc5fce4f6dfccad4c
Abhishek kumar das <ak2096565@gmail.com>
2025-08-22 23:16:08 +0530

    I create a rectangle on screen

--- code/main.py
+++ code/main.py
@@ -7,16 +7,28 @@ class Main:
         pygame.init()
         self.screen=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         
+        # game object
+        self.bg_rect=[pygame.Rect(COL*CELL_SIZE,0,CELL_SIZE,CELL_SIZE) for COL in range(0,COLN,2)]
+        
+    def draw_bg(self):
+        for rect in self.bg_rect:
+            pygame.draw.rect(self.screen, 'red', rect)
+            
+                
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-        
+            
+            # drawing code should be outside the event loop
             self.screen.fill(LIGHT_GREEN)
+            self.draw_bg()
             pygame.display.update()
-                
-        
+
+
+              
 main=Main()
 main.run()
+
